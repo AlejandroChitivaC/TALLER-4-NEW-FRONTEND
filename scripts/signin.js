7334e6c376a94537a28c73613979423715b30444
Alejandro Chitiva Castillo <83592808+AlejandroChitivaC@users.noreply.github.com>
2022-04-27 21:46:41 -0500

    new commit

--- scripts/signin.js
+++ scripts/signin.js
@@ -1,4 +1,24 @@
-
-const btn = document.getElementById("btn");
-const email = document.getElementById("email");
-const pass = document.getElementById("pass");
+let formulario = document.getElementById("ingresar");
+const url = "https://regusers.herokuapp.com/usarios";
+window.addEventListener("DOMContentLoaded", async () => { });
+formulario.addEventListener("submit", async (esc) => {
+  esc.preventDefault();
+  //capturando los datos del html y los voy a enviar a mi data.json
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("pass").value;
+  let resp = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify({
+      email: email,
+      password: password
+    }),
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+  });
+  let data = resp.json();
+  console.log(data);
+  alert("Bienvenido "+email);
+  window.location = "../web/perfil.html";
+  
+});
